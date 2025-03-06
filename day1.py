a6451887e94ecd1daf49c035ac7d3585f445ac2f
oreo <andriaroseaureo007@gmail.com>
2025-03-06 12:39:15 +0530

    day 4

---
+++ day1.py
@@ -0,0 +1,9 @@
+""" name = "Oreo"
+age = 22
+print("My name is", name)
+print("I am", age, "years old")"""
+
+
+name = input("what is your name?")
+age = input("what is your age?")
+print(f"Hi, im {name}, my age is {age}!")
---
+++ day2.py
@@ -0,0 +1,7 @@
+""" user name genarator"""
+name = str(input("enter yout name = "))
+age = int(input("enter your birth year = "))
+
+print(f"USERID = {name}{age}")
+
+
